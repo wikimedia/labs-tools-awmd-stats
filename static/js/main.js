229019ceb2b432471ae66c9a035ba40147b9dd79
African Hope <samuelguebo@gmail.com>
2018-03-01 01:58:08 +0000

    datepicker logic

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,18 @@ jQuery(document).ready( function($){
 		format: "mm-yyyy",
 		startView: "months", 
 		minViewMode: "months"
-	});
+	}).on('changeDate', function(e) {
+		var y = e.date.getFullYear(),
+            _m = e.date.getMonth() + 1,
+            m = (_m > 9 ? _m : '0'+_m),
+            _d = e.date.getDate(),
+            d = (_d > 9 ? _d : '0'+_d),
+            month = y + '-' + m;
+
+		console.log("open: "+ "/month/"+month)
+        //window.open("/month/"+month,"_self")
+    });
+
 
 
 	// progress bar while loading stats
