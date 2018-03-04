32367274b8aac808728c5fa85da800a0a8376cab
Alangi Derick <alangiderick@gmail.com>
2018-03-04 18:21:11 +0100

    Reindent
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -6,11 +6,11 @@
 # Checks if Python 3 and pip is installed before executing
 if command -v python3 &>/dev/null; then
 	echo -e "\n\033[1;32mChecking your system for Python 3..."
-    echo -e "\033[1;32mPython 3 is installed, checking pip..."
-    echo -e "\033[1;32mpip is installed, now installing dependencies...\n\033[0m"
+	echo -e "\033[1;32mPython 3 is installed, checking pip..."
+	echo -e "\033[1;32mpip is installed, now installing dependencies...\n\033[0m"
 else
-    echo -e "\033[1;32mPython 3 is not installed, aborting...\033[0m"
-    exit 1
+	echo -e "\033[1;32mPython 3 is not installed, aborting...\033[0m"
+	exit 1
 fi
 
 pip install -r requirements.txt
