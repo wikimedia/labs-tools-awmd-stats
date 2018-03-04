077fd83709b7d47c710a71e10c29c2bdc20cc150
Alangi Derick <alangiderick@gmail.com>
2018-03-04 18:18:52 +0100

    Adding color marks to install-dep.sh script
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -5,13 +5,14 @@
 
 # Checks if Python 3 and pip is installed before executing
 if command -v python3 &>/dev/null; then
-    echo -e "\nPython 3 is installed, checking pip..."
-    echo -e "pip is installed, now installing dependencies...\n"
+	echo -e "\n\033[1;32mChecking your system for Python 3..."
+    echo -e "\033[1;32mPython 3 is installed, checking pip..."
+    echo -e "\033[1;32mpip is installed, now installing dependencies...\n\033[0m"
 else
-    echo "Python 3 is not installed, aborting..."
+    echo -e "\033[1;32mPython 3 is not installed, aborting...\033[0m"
     exit 1
 fi
 
 pip install -r requirements.txt
 
-echo -e "\nYou can run the application now!"
+echo -e "\n\033[1;32mYou can run the application now!\033[0m"
