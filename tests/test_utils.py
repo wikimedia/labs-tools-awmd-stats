1bbd8fa7ee49e999e636b28a1da9d0bf5a76b4d4
Samuel Guebo <samuelguebo@gmail.com>
2018-08-24 11:44:10 +0000

    Add some unit tests for UD methods
    * readContributorsFromFile()
    * getContributorStats()
    * filterMonth()
    
    Bug: T202231
    Change-Id: Ic57c2f5858de7ecb6a13b8eab762b196752548ed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,10 @@ import pytest
 import time
 
 from awmdstats.utils import dbHasMonth
+from awmdstats.utils import filterMonth
+from awmdstats.utils import getContributorStats
 from awmdstats.utils import getCurrentMonth
+from awmdstats.utils import readContributorsFromFile
 
 
 @pytest.mark.usefixtures("testapp")
@@ -29,3 +32,25 @@ class TestUtils:
         #  Past month with no data: https://tools.wmflabs.org/awmd-stats/month/2017-11.
         status = dbHasMonth("2017-11")  # search month without data
         assert bool(status) is False  # returns False for month without data
+
+    # Test readContributorsFromFile() method
+    def testReadContributorsFromFile(self):
+        contributors = readContributorsFromFile()
+        # returns False if isn't a list or is empty
+        assert len(contributors) > 0 and isinstance(contributors, list)
+
+    # Test getContributorStats() method
+    def testGetContributorStats_WithData(self):
+        stats = getContributorStats("Rosalieper", "2018-07")
+        assert len(stats) > 0 and isinstance(stats, list)
+
+    def testGetContributorStats_WithoutData(self):
+        #  Past month with no data:
+        #  https://tools.wmflabs.org/awmd-stats/contributor/Rosalieper/2018-04.
+        stats = getContributorStats("BamLifa", "2018-04")
+        assert len(stats) < 1 and isinstance(stats, list)
+
+    # Test filterMonth() method
+    def testFilterMonth(self):
+        status = filterMonth("2018-07-14", "2018-07")
+        assert bool(status) is True  # returns True if month is found if full date time
